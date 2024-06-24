ced2befb72d2afd71c07785081a64edcd2e0f8a6
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-24 16:53:55 +0200

    feat: add email button

---
+++ src/components/App/EmailButton.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@/components/ui/Button'
+import { Typography } from '@/components/ui/Typography'
+import { emailAddress } from '@/constants/midia'
+import { useMenuContext } from '@/context/menuContext'
+import { useTranslate } from '@/hooks/useTranslate'
+import { FC } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const EmailBtn = styled(Button)`
+  padding: 0;
+`
+
+export const EmailButton: FC = () => {
+  const translate = useTranslate()
+  const text = translate.components.navbar
+  const { setShowMobile } = useMenuContext()
+
+  return (
+    <EmailBtn
+      $size='medium'
+      $variant='primary'
+      onClick={() => setShowMobile(false)}
+      aria-label='send email'
+      as={Link}
+      to={`mailTo:${emailAddress}`}
+    >
+      <Typography $isUpperCase={true} $margin='0.75rem 1.5rem'>
+        {text.buttons.contact}
+      </Typography>
+    </EmailBtn>
+  )
+}
