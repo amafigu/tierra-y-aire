b916f8be7ff5f69d5a5d4fee49307d32c322bb40
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-24 21:30:39 +0200

    feature: add whatsapp button

---
+++ src/components/App/WhatsAppButton.tsx
@@ -0,0 +1,58 @@
+import { whatsappUrl } from '@/constants/midia'
+import { useMenuContext } from '@/context/menuContext'
+import { useTranslate } from '@/hooks/useTranslate'
+import { laptop } from '@/styles/breakpoints' // Import your breakpoint
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FC } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { Flex } from '../ui/Flex'
+import { Typography } from '../ui/Typography'
+
+const WhatsAppBtn = styled(Flex)`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  transform: none;
+  border-radius: 2px;
+  background-color: ${(props) => props.theme.color.branding};
+  padding: ${(props) => props.theme.spacing.smallest}
+    ${(props) => props.theme.spacing.semiLarge};
+  color: ${(props) => props.theme.color.backgroundPrimary};
+
+  z-index: ${(props) => props.theme.zIndex.whatsappButton};
+
+  ${laptop`
+    position: fixed;
+    top: 50%;
+    right: -112px;
+    width: 218px;
+    height: 40px;
+    transform: translateY(-50%) rotate(90deg);
+    border-radius: ${(props) => props.theme.borderRadius.regular};
+  `}
+`
+
+export const WhatsAppButton: FC = () => {
+  const translate = useTranslate()
+  const text = translate.components.whatsAppButton
+  const { showMobile } = useMenuContext()
+
+  return (
+    !showMobile && (
+      <WhatsAppBtn
+        as={Link}
+        to={whatsappUrl}
+        $justifyContent='center'
+        $alignItems='center'
+        aria-label='send whatsapp'
+      >
+        <Flex $alignItems='center'>
+          <FontAwesomeIcon icon={faWhatsapp} size='2x' />
+          <Typography $margin='0 0 0 0.5rem'>{text.text}</Typography>
+        </Flex>
+      </WhatsAppBtn>
+    )
+  )
+}
