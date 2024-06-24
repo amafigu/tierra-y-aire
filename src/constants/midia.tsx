5d42556b75ad22ace99595c670e80cda945463e1
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-24 21:29:14 +0200

    add constants for images and midia urls

---
+++ src/constants/midia.tsx
@@ -0,0 +1,6 @@
+export const instagramUrl = 'https://www.instagram.com/rolo.veron/'
+export const facebookUrl = 'https://www.facebook.com/rolo.veron.3'
+export const youtubeUrl =
+  'https://www.youtube.com/channel/UCAHUUHVEh7ov0qv8FIE11ZQ'
+export const whatsappUrl = 'https://api.whatsapp.com/send?phone=5493413223169'
+export const emailAddress = 'percusion.tierrayaire@gmail.com'
