7d927a19d18a587ce555712f0577d1581e91dd32
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-25 11:26:51 +0200

    feat: add text-align prop to Typography

--- src/components/ui/Typography.tsx
+++ src/components/ui/Typography.tsx
@@ -1,4 +1,6 @@
-import { styled } from 'styled-components'
+import { CSSProperties, styled } from 'styled-components'
+
+import { Color, colors } from '@/constants/colors'
 
 import type { FontSize } from '@/constants/fontSizes'
 import { fontSizes } from '@/constants/fontSizes'
@@ -7,17 +9,28 @@ import type { FontWeight } from '@/constants/fontWeights'
 import { fontWeights } from '@/constants/fontWeights'
 
 type Props = {
-  $family?: 'serif' | 'sansSerif'
+  $overflow?: CSSProperties['overflow']
+  $margin?: CSSProperties['margin']
+  $padding?: CSSProperties['padding']
+  $family?: 'serif' | 'sansSerif' | 'primary'
   $size?: FontSize
   $weight?: FontWeight
+  $color?: Color
   $isUpperCase?: boolean
+  $style?: CSSProperties['fontStyle']
+  $textAlign?: CSSProperties['textAlign']
 }
 
 export const Typography = styled.span<Props>`
-  font-weight: ${(props) =>
-    props.$weight ? fontWeights[props.$weight] : 'inherit'};
-  font-family: ${(props) =>
-    props.$family ? props.theme.font[props.$family] : 'inherit'};
-  font-size: ${(props) => (props.$size ? fontSizes[props.$size] : 'inherit')};
+  line-height: 128%;
+  font-weight: ${(props) => props.$weight && fontWeights[props.$weight]};
+  font-family: ${(props) => props.$family};
+  font-size: ${(props) => props.$size && fontSizes[props.$size]};
+  margin: ${(props) => props.$margin};
+  padding: ${(props) => props.$padding};
+  overflow: ${(props) => props.$overflow};
+  color: ${(props) => (props.$color ? colors[props.$color] : 'inherit')};
   text-transform: ${(props) => (props.$isUpperCase ? 'uppercase' : 'inherit')};
+  font-style: ${(props) => props.$style};
+  text-align: ${(props) => props.$textAlign};
 `
