6dafb6f9a156fd31df64e6c6ec21de9c762eb0a2
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-25 12:11:47 +0200

    refactor: add variants and sizes to Button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -6,14 +6,30 @@ const sizeStyle = {
   small: css`
     font-size: ${fontSizes.small};
     padding: 0.5rem 1rem;
+    border-radius: ${(props) => props.theme.borderRadius.small};
+  `,
+  smallRounded: css`
+    font-size: ${fontSizes.small};
+    padding: 0.5rem 1rem;
     border-radius: 1rem;
   `,
+
   medium: css`
+    font-size: ${fontSizes.medium};
+    padding: 0.75rem 1.5rem;
+    border-radius: ${(props) => props.theme.borderRadius.regular};
+  `,
+  mediumRounded: css`
     font-size: ${fontSizes.medium};
     padding: 0.75rem 1.5rem;
     border-radius: 1.5rem;
   `,
   huge: css`
+    font-size: ${fontSizes.large};
+    padding: 1rem 2rem;
+    border-radius: ${(props) => props.theme.borderRadius.large};
+  `,
+  hugeRounded: css`
     font-size: ${fontSizes.large};
     padding: 1rem 2rem;
     border-radius: 2rem;
@@ -62,7 +78,6 @@ const variantStyle = {
       background-color: ${(props) => props.theme.color.branding};
       border-color: ${(props) => props.theme.color.strockeSecondary};
       border-width: 2px;
-      border-style: solid;
     }
   `,
   link: css`
@@ -73,7 +88,21 @@ const variantStyle = {
     }
 
     &:active {
-      color: ${(props) => props.theme.color.dark};
+      color: ${(props) => props.theme.color.primary};
+    }
+  `,
+  card: css`
+    color: ${(props) => props.theme.color.primary};
+    border-width: 1px;
+    border-style: solid;
+
+    borde-color: ${(props) => props.theme.color.primary};
+    &:hover {
+      color: ${(props) => props.theme.color.branding};
+    }
+
+    &:active {
+      color: ${(props) => props.theme.color.primary};
     }
   `,
 }
@@ -81,7 +110,13 @@ const variantStyle = {
 type Variant = keyof typeof variantStyle
 
 type Props = {
-  $size: 'small' | 'medium' | 'huge'
+  $size:
+    | 'small'
+    | 'smallRounded'
+    | 'medium'
+    | 'mediumRounded'
+    | 'huge'
+    | 'hugeRounded'
   $variant: Variant
 }
 
@@ -89,9 +124,9 @@ export const Button = styled.button<Props>`
   cursor: pointer;
   display: flex;
   align-items: center;
-  justify-content:center
-    ${(props) => sizeStyle[props.$size]}
-    ${(props) => variantStyle[props.$variant]}
+  justify-content: center;
+  ${(props) => sizeStyle[props.$size]}
+  ${(props) => variantStyle[props.$variant]}
     &:focus-visible {
     outline: none;
   }
