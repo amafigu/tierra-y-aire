30ca19dce293375cdbfe6e76e25680ec2510151f
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-25 12:07:36 +0200

    add testimonials and translation

---
+++ src/constants/testimonials.tsx
@@ -0,0 +1,48 @@
+export interface TestimonialProps {
+  name: string
+  image: string
+}
+
+export const TESTIMONIALS = [
+  {
+    name: 'mikeTiseyra',
+    image: 'testmonial_1.jpg',
+  },
+  {
+    name: 'silvinaRaquel',
+    image: 'testmonial_2.jpg',
+  },
+  {
+    name: 'luisa',
+    image: 'testmonial_3.jpg',
+  },
+  {
+    name: 'mumi',
+    image: 'testmonial_4.jpg',
+  },
+  {
+    name: 'marito',
+    image: 'testmonial_5.jpg',
+  },
+  {
+    name: 'carlitos',
+    image: 'testmonial_6.jpg',
+  },
+  {
+    name: 'cheliCastellano',
+    image: 'testmonial_7.jpg',
+  },
+  {
+    name: 'silvinaPesaresi',
+    image: 'testmonial_8.jpg',
+  },
+  {
+    name: 'flavia',
+    image: 'testmonial_9.jpg',
+  },
+
+  {
+    name: 'lilianaMartinez',
+    image: 'testmonial_10.jpg',
+  },
+]
