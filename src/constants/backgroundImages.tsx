955c438e34941dc46ba5aba175c5ab4cdd03a936
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-25 12:11:30 +0200

    add background images

---
+++ src/constants/backgroundImages.tsx
@@ -0,0 +1,7 @@
+export const musicalLanguageBackground = '/bannerAbout.png'
+export const philosophyBackground = '/philosophyBackground.png'
+export const educationBanner = '/educationBanner.png'
+export const ensembleBackground = '/ensembleBackground.png'
+export const musicalDirectionAndHandTechniqueBackground =
+  '/musicalDirectionAndHandTechniqueBackground.png'
+export const stickTechniqueBackground = '/stickTechniqueBackground.png'
