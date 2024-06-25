5113aaf5635c9c6c2f76923be36c1ce09054f029
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-25 12:13:50 +0200

    add navigate hook

---
+++ src/hooks/useOnNavigate.tsx
@@ -0,0 +1,14 @@
+import { useNavigate } from 'react-router-dom'
+
+type Setter = (value: boolean) => void
+
+export const useOnNavigate = () => {
+  const navigate = useNavigate()
+
+  const onNavigate = (setters: Setter[], route: string) => {
+    setters.forEach((setter) => setter(false))
+    navigate(route)
+  }
+
+  return { onNavigate }
+}
