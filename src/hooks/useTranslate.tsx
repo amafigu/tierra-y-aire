36495b1fd68c56c2524bfe1ae206786c097ad507
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-11 17:40:16 +0200

    add properties to return in useTranslate

--- src/hooks/useTranslate.tsx
+++ src/hooks/useTranslate.tsx
@@ -5,5 +5,5 @@ export const useTranslate = (): Translations => {
   const { translate, language, setLanguage } = useLocaleContext()
   const components = translate.components;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return { translate: translate as Translations,  language, setLanguage, components, };
+  return { translate: translate as Translations,  language, setLanguage, components };
 };
