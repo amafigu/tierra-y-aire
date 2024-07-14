afdc20b1c4099d18149fce18f2a8869d320203d6
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-14 10:25:57 +0200

    language select and navbar tests

--- src/components/App/LanguagesSelector.test.tsx
+++ src/components/App/LanguagesSelector.test.tsx
@@ -1,36 +1,13 @@
-import { LocaleContext, LocaleContextTypes } from '@/context/localeContext'
-import { MenuContext, MenuContextTypes } from '@/context/menuContext'
+import { LocaleContextTypes } from '@/context/localeContext'
+import { MenuContextTypes } from '@/context/menuContext'
 import en from '@/i18n/en.json'
 import es from '@/i18n/es.json'
-import { theme } from '@/styles/theme'
-import { fireEvent, render, screen } from '@testing-library/react'
-import { ReactElement } from 'react'
-import { ThemeProvider } from 'styled-components'
+import { customRender } from '@/test-utils'
+import { fireEvent, screen } from '@testing-library/react'
 import { vi } from 'vitest'
 import { LanguagesSelector } from './LanguagesSelector'
 
 const languages = { en, es }
-
-const customRender = (
-  ui: ReactElement,
-  {
-    localeProviderProps,
-    menuProviderProps,
-  }: {
-    localeProviderProps: Partial<LocaleContextTypes>
-    menuProviderProps: Partial<MenuContextTypes>
-  },
-) => {
-  return render(
-    <ThemeProvider theme={theme}>
-      <LocaleContext.Provider value={localeProviderProps as LocaleContextTypes}>
-        <MenuContext.Provider value={menuProviderProps as MenuContextTypes}>
-          {ui}
-        </MenuContext.Provider>
-      </LocaleContext.Provider>
-    </ThemeProvider>,
-  )
-}
 
 describe('<LanguagesSelector />', () => {
   let localeProviderProps: Partial<LocaleContextTypes>
@@ -60,6 +37,7 @@ describe('<LanguagesSelector />', () => {
     })
 
     const spanishButton = screen.getByText('ES')
+
     fireEvent.click(spanishButton)
 
     expect(localeProviderProps.setLanguage).toHaveBeenCalledWith('es')
