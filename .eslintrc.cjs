6de56b25a2058ce6a00886f7cf1b1b8d3b65a58b
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-14 10:18:52 +0200

    add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,25 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        jsxSingleQuote: true,
+        trailingComma: 'all',
+        semi: false,
+        printWidth: 80,
+      },
     ],
   },
 }
