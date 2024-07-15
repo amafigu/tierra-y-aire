59ae9b9677cbfbc51aaf914c322ee9885361ee50
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-07-15 14:09:12 +0200

    add initial entry in navbar test

--- src/components/App/Navbar.test.tsx
+++ src/components/App/Navbar.test.tsx
@@ -1,11 +1,14 @@
+import { ROUTES } from '@/constants/routes'
 import { LocaleContextTypes } from '@/context/localeContext'
 import { MenuContextTypes } from '@/context/menuContext'
 import en from '@/i18n/en.json'
 import es from '@/i18n/es.json'
 import { About } from '@/pages/About'
+import { EnsembleCourses } from '@/pages/courses/EnsembleCourses'
 import { customRender } from '@/test-utils'
 import { fireEvent, screen } from '@testing-library/react'
 import { createMemoryHistory, History } from 'history'
+import { Route, Routes } from 'react-router-dom'
 import { vi } from 'vitest'
 import { Navbar } from './Navbar'
 
@@ -31,31 +34,32 @@ describe('<Navbar />', () => {
       showCourses: false,
       setShowCourses: vi.fn(),
     }
-    history = createMemoryHistory({ initialEntries: ['/'] })
+    history = createMemoryHistory({
+      initialEntries: [ROUTES.COURSES.ENSEMBLE, ROUTES.ABOUT],
+    })
   })
 
   it('navigates to about page by clicking on about link', () => {
-    customRender(<Navbar />, {
-      localeProviderProps,
-      menuProviderProps,
-      history,
-    })
+    customRender(
+      <>
+        <Navbar />
+        <Routes>
+          <Route path={ROUTES.COURSES.ENSEMBLE} element={<EnsembleCourses />} />
+          <Route path={ROUTES.ABOUT} element={<About />} />
+        </Routes>
+      </>,
+      {
+        localeProviderProps,
+        menuProviderProps,
+        history,
+      },
+    )
 
     const aboutLink = screen.getByText(/About us/i)
 
     fireEvent.click(aboutLink)
 
     expect(history.location.pathname).toBe('/about')
-
-    customRender(
-      <>
-        <About />
-      </>,
-      {
-        localeProviderProps,
-        menuProviderProps,
-      },
-    )
 
     const aboutText = screen.getByText(
       /established since 2018, the Tierra y Aire ethos has always been one of integration and sharing. Today, we have become the people’s choice for drum education as the largest specialized drum school in Rosario./i,
